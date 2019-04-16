6cde54b7a5ac62c1b0b13a3cb8a0db6ac279243f
steward0306 <asd265126@gmail.com>
2019-04-16 19:13:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText tex1,tex2,tex3;
+    private TextView bmi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void BMI(View view){
+
+        double a;
+        String b;
+        tex1 = findViewById(R.id.editText1);
+        tex2 = findViewById(R.id.editText2);
+        tex3 = findViewById(R.id.editText3);
+        bmi = findViewById(R.id.textView);
+
+        a = Double.parseDouble(tex1.getText().toString())/Math.pow(Double.parseDouble(tex2.getText().toString()),2);
+        b = Double.toString(a);
+
+        bmi.setText(b);
+
+
+
+
+
+    }
+
+}
